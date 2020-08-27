f08f28d2742eaeb319cebeb00fd46dc5244370ad
Antonios Papadakis <1001545@isd535.org>
2020-08-27 01:43:56 -0500

    Add Comments Where Necessary
    macOS

--- macos/install.sh
+++ macos/install.sh
@@ -6,6 +6,7 @@ function setup {
     read -p 'Which PHP version would you like to install?[latest-7.1/latest-7.2/latest-7.3/default: latest-7.4/latest-8.0/latest-master/q (quit)] ' PHPINSTALLVERSION
     PHPINSTALLVERSION=${PHPINSTALLVERSION:-latest-7.4}
 
+    # Setup Correct files depending on PHP installation version choice
     if [ "$PHPINSTALLVERSION" = latest-7.1 ]; then
         cd ../releases/ || exit
         unzip php-src-php-7.1.32.zip -d latest-7.1
@@ -56,6 +57,7 @@ function setup {
         exit 130
     fi
 
+    # Install dependencies using Homebrew
     echo Installing Dependencies
     brew install flex autoconf automake libtool re2c bison openssl curl enchant gd freetype mhash libiconv libsodium libjpeg pcre libxml2 argon2 tidy-html5 libzip
     # Export Packages to the $PATH so They can be Found by the System
@@ -63,6 +65,7 @@ function setup {
     source ~/.bash_profile
     echo Installed all Dependencies
 
+    # Go into the right directory depending on PHP installation version
     if [ "$PHPINSTALLVERSION" = latest-7.1 ]; then
         cd php-src-php-7.1.32/ || exit
     elif [ "$PHPINSTALLVERSION" = latest-7.2 ]; then
@@ -81,10 +84,12 @@ function setup {
 # Configure PHP (./buildconf and ./configure)
 function configure {
     echo Building configuration script
+    # Build the configure script
     sudo ./buildconf --force || exit
     echo Built Configuration Script
     # Configure PHP Installation
     echo Configuring PHP
+    # Configure PHP per install version choice
     if [ "$PHPINSTALLVERSION" = latest-8.0 ] || [ "$PHPINSTALLVERSION" = latest-master ]; then
         sudo ./configure --with-openssl --enable-bcmath --enable-calendar --with-curl --with-enchant --enable-ftp --enable-gd --with-webp --with-jpeg --with-freetype --with-mhash --with-iconv=/usr/local/Cellar/libiconv/1.16 --with-imap-ssl --enable-mbstring --enable-pdo --with-pdo-mysql=mysqlnd --enable-shmop --enable-soap --enable-sockets --with-sodium --with-password-argon2 --enable-sysvmsg --enable-sysvsem --enable-sysvshm --with-tidy --with-zip || exit
     elif [ "$PHPINSTALLVERSION" = latest-7.4 ]; then
@@ -103,11 +108,13 @@ function configure {
 function build {
     # Set the Job Count for make
     echo Setting job count for make
+    # Set the make job count for the lowest buid time
     THREADCOUNT=$(sysctl -n hw.ncpu)
     JOBCOUNT=$((THREADCOUNT+1))
     echo Set job count for make
     # Build PHP
     echo Building PHP
+    # Build PHP using make
     sudo make -j"$JOBCOUNT" || exit
     echo Built PHP
 }
@@ -115,6 +122,7 @@ function build {
 # Test PHP (make test)
 function test {
     echo Testing PHP
+    # Test PHP per PHP version choice
     if [ "$PHPINSTALLVERSION" = latest-8.0 ] || [ "$PHPINSTALLVERSION" = latest-master ] || [ "$PHPINSTALLVERSION" = latest-7.4 ]; then
         sudo make TEST_PHP_ARGS=-j"$JOBCOUNT" test
     elif [ "$PHPINSTALLVERSION" = latest-7.3 ]; then
@@ -130,14 +138,17 @@ function test {
 
 # Install PHP CLI (make install)
 function installCLI {
+    # Ask the user if the PHP CLI should be installed or not
     read -p 'The tests have been completed and you may or may not have had failures on some of them. This can sometimes be ignored with the latest builds, or may need extra attention. Would you like to continue with installation?[default: y/Y/n/N] ' ERRORRESPONSE
     ERRORRESPONSE=${ERRORRESPONSE:-y}
     if [ "$ERRORRESPONSE" = y ] || [ "$ERRORRESPONSE" = Y ]; then
+        # Install PHP CLI
         echo Installing PHP-CLI
         sudo make install || exit
         echo Installed PHP-CLI
         exit 0
     elif [ "$ERRORRESPONSE" = n ] || [ "$ERRORRESPONSE" = N ]; then
+        # Cancel install, go back to setup
         echo cancelled installation
         git checkout master
         setup
@@ -148,6 +159,7 @@ function installCLI {
     BREWCHECK=$?
 
 if [ "$BREWCHECK" = 0 ]; then
+    # Install PHP CLI
     setup
     configure
     build
@@ -158,15 +170,19 @@ elif [ "$BREWCHECK" = 127 ]; then
     echo installing Homebrew
     sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     echo Homebrew has been installed
+    # Install PHP CLI
     setup
     configure
     build
     test
     installCLI
 else
+    # Error Out
     echo There was an error checking for Homebrew support.
     git checkout master
     exit 1
 fi
+
+# Reset to previous state
 git checkout master
 exit 0
