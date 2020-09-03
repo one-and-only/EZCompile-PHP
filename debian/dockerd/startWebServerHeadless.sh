a8dce15594a89f45b016f6b41e5a7c5473eacf95
Antonios P <43624528+one-and-only@users.noreply.github.com>
2020-09-03 13:49:58 -0500

    remove all space (%20) characters
    Add Debian Docker Support
    
    Debian
    
    Simplified Starting Process for Docker
    
    Debian

---
+++ debian/dockerd/startWebServerHeadless.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$PIDOF" = : ]; then
+        sudo dockerd &
+        exit 0
+else
+        echo The Docker Daemon is Already Running, no Need to Start!
+        exit 0
+fi
+
+cd ../dockerFiles || exit
+docker-compose up -d
