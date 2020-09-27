be75250222923c54ddc9c2d7896a39761e3a79e0
one-and-only <antonios@pop-os.localdomain>
2020-09-27 18:13:25 -0500

    Add Support for Debian (New Format)
    Debian

---
+++ debian/Docker/dockerFiles/mysql-5.7/php-latest-7.3/startMySQL.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NC='\033[0m' # No Color
+YELLOW='\033[1;33m'
+RED='\033[1;31m'
+
+function SQLSetup() {
+    read -p 'Where would you like your MySQL server files to reside on your local machine (absolute path, must exist)? ' MYSQLFOLDERLOCATION
+    ls "$MYSQLFOLDERLOCATION"
+    ISMYSQLFOLDERPRESENT=$?
+    if [ -z "$MYSQLFOLDERLOCATION" ]; then
+        echo -e "${YELLOW}The path of the MySQL file folder cannot be empty. Please choose a folder.${NC}"
+        SQLSetup
+    elif [ "$ISMYSQLFOLDERPRESENT" = 2 ]; then
+        echo -e "$MYSQLFOLDERLOCATION" "${RED}"does not exist, please choose another folder."${NC}"
+        SQLSetup
+    fi
+}
+
+SQLSetup
+
+if [ ! "$(docker ps -q -f name=mysql57)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=mysql57)" ]; then
+        # cleanup
+        sudo docker rm mysql57
+    fi
+    # run your container
+    sudo docker run -d -p 3306:3306 -p 33060:33060 --volume "$MYSQLFOLDERLOCATION":/var/lib/mysql --name mysql57 frostedflakez/php-mysql-webserver:0.9-beta.3-mysql-latest-5.7
+fi
---
+++ debian/Docker/dockerd/installDocker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add || exit
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" || exit
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y || exit
+echo adding user to docker group
+sudo usermod -aG docker $USER
+echo applying changes
+newgrp docker
